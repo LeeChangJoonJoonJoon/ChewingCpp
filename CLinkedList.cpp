dceee112f03fbc9ff3b02e2d48176e1a56c957fb
LeeChangJoonJoonJoon <hooninpark@e8igh.co.kr>
2024-08-11 17:52:25 +0900

    gonna make linked list (ll) with template func

--- CLinkedList.cpp
+++ CLinkedList.cpp
@@ -2,4 +2,4 @@
 // Created by Changjoon Lee on 8/11/24.
 //
 
-#include "LinkedList.h"
+#include "CLinkedList.h"
---
+++ CLinkedList.h
@@ -0,0 +1,15 @@
+//
+// Created by Changjoon Lee on 8/11/24.
+//
+
+#ifndef CHEWINGCPP_CLINKEDLIST_H
+#define CHEWINGCPP_CLINKEDLIST_H
+
+
+class CLinkedList
+{
+
+};
+
+
+#endif //CHEWINGCPP_CLINKEDLIST_H
